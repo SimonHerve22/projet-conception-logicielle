6bd9e200250d3c73d38daea00407a18ad6e26e7c
bbrunel <bastien.brunel@eleve.ensai.fr>
2026-01-19 15:37:34 +0100

    pyproject.toml + main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+from dotenv import load_dotenv
 from utils.environment_printer import EnvironmentPrinter
 
 if __name__ == "__main__":
+    load_dotenv(dotenv_path='.env.local')
     EnvironmentPrinter.print_environment_variables()
