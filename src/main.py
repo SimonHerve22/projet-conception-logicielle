f33133ac53d4899a44f0225705c0e09d318a19d5
SimonHerve22 <id2662@ensai.fr>
2026-01-27 15:01:44 +0100

    Début de programmation avec scrapping de données Fandom

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
+
 from utils.environment_printer import EnvironmentPrinter
 
+
 if __name__ == "__main__":
-    load_dotenv(dotenv_path='.env.local')
+    load_dotenv(dotenv_path=".env.local")
     EnvironmentPrinter.print_environment_variables()
