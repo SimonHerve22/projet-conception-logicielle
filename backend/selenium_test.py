f76b242d2aefe1cbc92706e53906d45df6a42838
SimonHerve22 <id2662@ensai.fr>
2026-02-15 11:19:39 +0100

    Scraping qui fonctionne, pour les statistiques des joueurs, et pour toutes les saisons de LEC

--- backend/selenium_test.py
+++ backend/selenium_test.py
@@ -1,54 +1,140 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pandas as pd
+import os
+import time
+import requests
 
 
-liste_tournois = ['EU_LCS/Season_3/Spring_Season', 'EU_LCS/Season_3/Spring_Playoffs', 'EU_LCS/Season_3/Summer_Season', 'EU_LCS/Season_3/Summer_Playoffs',
-'EU_LCS/2014_Season/Spring_Season', 'EU_LCS/2014_Season/Spring_Playoffs', 'EU_LCS/2014_Season/Summer_Season', 'EU_LCS/2014_Season/Summer_Playoffs',
-'EU_LCS/2015_Season/Spring_Season', 'EU_LCS/2015_Season/Spring_Playoffs', 'EU_LCS/2015_Season/Summer_Season', 'EU_LCS/2015_Season/Summer_Playoffs',
-'EU_LCS/2016_Season/Spring_Season', 'EU_LCS/2016_Season/Spring_Playoffs', 'EU_LCS/2016_Season/Summer_Season', 'EU_LCS/2016_Season/Summer_Playoffs',
-'EU_LCS/2017_Season/Spring_Season', 'EU_LCS/2017_Season/Spring_Playoffs', 'EU_LCS/2017_Season/Summer_Season', 'EU_LCS/2017_Season/Summer_Playoffs',
-'EU_LCS/2018_Season/Spring_Season', 'EU_LCS/2018_Season/Spring_Playoffs', 'EU_LCS/2018_Season/Summer_Season', 'EU_LCS/2018_Season/Summer_Playoffs',
-'LEC/2019_Season/Spring_Season', 'LEC/2019_Season/Spring_Playoffs', 'LEC/2019_Season/Summer_Season', 'LEC/2019_Season/Summer_Playoffs',
-'LEC/2020_Season/Spring_Season', 'LEC/2020_Season/Spring_Playoffs', 'LEC/2020_Season/Summer_Season', 'LEC/2020_Season/Summer_Playoffs',
-'LEC/2021_Season/Spring_Season', 'LEC/2021_Season/Spring_Playoffs', 'LEC/2021_Season/Summer_Season', 'LEC/2021_Season/Summer_Playoffs',
-'LEC/2022_Season/Spring_Season', 'LEC/2022_Season/Spring_Playoffs', 'LEC/2022_Season/Summer_Season', 'LEC/2022_Season/Summer_Playoffs',
-'LEC/2023_Season/Winter_Season', 'LEC/2023_Season/Winter_Groups', 'LEC/2023_Season/Winter_Playoffs',
-'LEC/2023_Season/Spring_Season', 'LEC/2023_Season/Spring_Groups', 'LEC/2023_Season/Spring_Playoffs',
-'LEC/2023_Season/Summer_Season', 'LEC/2023_Season/Summer_Groups', 'LEC/2023_Season/Summer_Playoffs',
-'LEC/2024_Season/Winter_Season', 'LEC/2024_Season/Winter_Playoffs',
-'LEC/2024_Season/Spring_Season', 'LEC/2024_Season/Spring_Playoffs',
-'LEC/2024_Season/Summer_Season', 'LEC/2024_Season/Summer_Playoffs',
-'LEC/2025_Season/Winter_Season', 'LEC/2025_Season/Winter_Playoffs',
-'LEC/2025_Season/Spring_Season', 'LEC/2025_Season/Spring_Playoffs',
-'LEC/2025_Season/Summer_Season', 'LEC/2025_Season/Summer_Playoffs',
-'LEC/2026_Season/Versus_Season', 'LEC/2026_Season/Versus_Playoffs']
 
-liste_pages = ['Overview', 'Team Rosters', 'Picks & Bans', 'Match History', 'Champion Stats', 'Player Stats']
+liste_tournois = [
+    "EU_LCS/Season_3/Spring_Season",
+    "EU_LCS/Season_3/Spring_Playoffs",
+    "EU_LCS/Season_3/Summer_Season",
+    "EU_LCS/Season_3/Summer_Playoffs",
+    "EU_LCS/2014_Season/Spring_Season",
+    "EU_LCS/2014_Season/Spring_Playoffs",
+    "EU_LCS/2014_Season/Summer_Season",
+    "EU_LCS/2014_Season/Summer_Playoffs",
+    "EU_LCS/2015_Season/Spring_Season",
+    "EU_LCS/2015_Season/Spring_Playoffs",
+    "EU_LCS/2015_Season/Summer_Season",
+    "EU_LCS/2015_Season/Summer_Playoffs",
+    "EU_LCS/2016_Season/Spring_Season",
+    "EU_LCS/2016_Season/Spring_Playoffs",
+    "EU_LCS/2016_Season/Summer_Season",
+    "EU_LCS/2016_Season/Summer_Playoffs",
+    "EU_LCS/2017_Season/Spring_Season",
+    "EU_LCS/2017_Season/Spring_Playoffs",
+    "EU_LCS/2017_Season/Summer_Season",
+    "EU_LCS/2017_Season/Summer_Playoffs",
+    "EU_LCS/2018_Season/Spring_Season",
+    "EU_LCS/2018_Season/Spring_Playoffs",
+    "EU_LCS/2018_Season/Summer_Season",
+    "EU_LCS/2018_Season/Summer_Playoffs",
+    "LEC/2019_Season/Spring_Season",
+    "LEC/2019_Season/Spring_Playoffs",
+    "LEC/2019_Season/Summer_Season",
+    "LEC/2019_Season/Summer_Playoffs",
+    "LEC/2020_Season/Spring_Season",
+    "LEC/2020_Season/Spring_Playoffs",
+    "LEC/2020_Season/Summer_Season",
+    "LEC/2020_Season/Summer_Playoffs",
+    "LEC/2021_Season/Spring_Season",
+    "LEC/2021_Season/Spring_Playoffs",
+    "LEC/2021_Season/Summer_Season",
+    "LEC/2021_Season/Summer_Playoffs",
+    "LEC/2022_Season/Spring_Season",
+    "LEC/2022_Season/Spring_Playoffs",
+    "LEC/2022_Season/Summer_Season",
+    "LEC/2022_Season/Summer_Playoffs",
+    "LEC/2023_Season/Winter_Season",
+    "LEC/2023_Season/Winter_Groups",
+    "LEC/2023_Season/Winter_Playoffs",
+    "LEC/2023_Season/Spring_Season",
+    "LEC/2023_Season/Spring_Groups",
+    "LEC/2023_Season/Spring_Playoffs",
+    "LEC/2023_Season/Summer_Season",
+    "LEC/2023_Season/Summer_Groups",
+    "LEC/2023_Season/Summer_Playoffs",
+    "LEC/2024_Season/Winter_Season",
+    "LEC/2024_Season/Winter_Playoffs",
+    "LEC/2024_Season/Spring_Season",
+    "LEC/2024_Season/Spring_Playoffs",
+    "LEC/2024_Season/Summer_Season",
+    "LEC/2024_Season/Summer_Playoffs",
+    "LEC/2025_Season/Winter_Season",
+    "LEC/2025_Season/Winter_Playoffs",
+    "LEC/2025_Season/Spring_Season",
+    "LEC/2025_Season/Spring_Playoffs",
+    "LEC/2025_Season/Summer_Season",
+    "LEC/2025_Season/Summer_Playoffs",
+    "LEC/2026_Season/Versus_Season",
+    "LEC/2026_Season/Versus_Playoffs",
+]
+# Dossier de sortie
+os.makedirs("exports_lec", exist_ok=True)
 
 driver = webdriver.Chrome()
-driver.get(
-"https://lol.fandom.com/wiki/LEC/2026_Season/Versus_Season/Player_Statistics"
-)
-driver.implicitly_wait(10)
+all_data = {}
+for tournoi in liste_tournois:
+    try:
+        print(f"Scraping : {tournoi}")
 
-table_html = driver.find_element(
-By.CSS_SELECTOR, "#mw-content-text table.wikitable"
-).get_attribute("outerHTML")
+        url = "https://lol.fandom.com/api.php"
+        params = {
+            "action": "parse",
+            "page": f"{tournoi}/Player_Statistics",
+            "prop": "text",
+            "format": "json"
+        }
 
-driver.quit()
+        res = requests.get(url, params=params)
+        res_json = res.json()
 
-soup = BeautifulSoup(table_html, "html.parser")
+        # ✔ gestion des pages absentes
+        if "parse" not in res_json:
+            print(f"❌ Page absente : {tournoi}")
+            continue
 
-rows = soup.find_all("tr")
+        html = res_json["parse"]["text"]["*"]
+        soup = BeautifulSoup(html, "html.parser")
 
-data = []
-for row in rows[1:]: # skip header
-    cells = [cell.get_text(strip=True) for cell in row.find_all(["th", "td"])]
-if cells:
-    data.append(cells)
+        table = soup.select_one("table.wikitable")
+        if table is None:
+            print(f"❌ Pas de table : {tournoi}")
+            continue
 
-# Exemple : afficher la première ligne joueur
-print(data[0])
+        rows = table.select("tr")
+        data = []
 
-print(data)
+        for row in rows[1:]:
+            cells = [c.get_text(strip=True) for c in row.select("th, td")]
+            if cells:
+                data.append(cells)
+
+        df = pd.DataFrame(data)
+        df = df.iloc[4:]
+        df.columns = [ "ID", "Name", "Games", "Wins", "Loses", "Winrate", "Kills/Game", "Deaths/Game", "Assists/Game", "KDA", "CS/Game", "CS/Min", "Vision/Game", "Vision/Min", "Golds/Game", "Golds/Min", "Damage/Game", "Damage/Min", "Kill Participation", "Kill Shara", "Gold Share", "Nombre Champions", "Champs", ]
+
+        # sauvegarde par feuille
+        all_data[tournoi.replace("/", "_")] = df
+
+    except Exception as e:
+        print(f"Erreur pour {tournoi} : {e}")
+
+# ✔ Export Excel multi-feuilles
+with pd.ExcelWriter("stats_lec.xlsx", engine="openpyxl") as writer:
+    for sheet, df in all_data.items():
+        df.to_excel(writer, sheet_name=sheet[:31], index=False)
+        worksheet = writer.sheets[sheet[:31]] 
+        for column in worksheet.columns: 
+            max_length = 0 
+            column_letter = column[0].column_letter 
+            for cell in column: 
+                if cell.value: 
+                    max_length = max(max_length, len(str(cell.value))) 
+            worksheet.column_dimensions[column_letter].width = max_length + 2
+
+print("✔ Terminé")
