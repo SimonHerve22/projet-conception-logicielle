d482ea754c79513f8eecd20f04d5aa4b404e01d6
marinepac123 <marinepachy@gmail.com>
2026-03-04 15:32:24 +0000

    suit eview et erreur import InquirerPy

---
+++ backend/main_essaie.py
@@ -0,0 +1,38 @@
+import logging
+
+import dotenv
+from utils.log_init import initialiser_logs
+from views.accueil.accueil_vue import AccueilVue
+
+
+if __name__ == "__main__":
+    dotenv.load_dotenv(override=True)
+
+    initialiser_logs("Application")
+
+    vue_courante = AccueilVue("Bienvenue sur ENS-all-in")
+    nb_erreurs = 0
+
+    while vue_courante:
+        if nb_erreurs > 100:
+            print("Le programme recense trop d'erreurs et va s'arrêter")
+            break
+        try:
+            # Affichage du menu
+            vue_courante.afficher()
+
+            # Affichage des choix possibles
+            vue_courante = vue_courante.choisir_menu()
+        except Exception as e:
+            logging.error(f"{type(e).__name__} : {e}", exc_info=True)
+            nb_erreurs += 1
+            vue_courante = AccueilVue(
+                "Une erreur est survenue, retour au menu principal.\n"
+                "Consultez les logs pour plus d'informations."
+            )
+
+    # Lorsque l'on quitte l'application
+    print("----------------------------------")
+    print("Au revoir")
+    for _ in range(3):
+        print()
---
+++ backend/views/accueil/inscription_vue.py
@@ -0,0 +1,34 @@
+import os
+
+from inquirerPy import inquirer
+import requests
+from view.vue_abstraite import VueAbstraite
+
+
+host = os.environ["HOST_WEBSERVICE"]
+END_POINT = "/utilisateur/"
+
+
+class InscriptionVue(VueAbstraite):
+    def choisir_menu(self):
+        # Demande à l'utilisateur de saisir pseudo, mot de passe...
+        pseudo = inquirer.text(message="Entrez votre pseudo : ").execute()
+        valide = False
+        while not valide:
+            mot_de_passe = inquirer.text(message="Entrez votre mot de passe : ").execute()
+            mot_de_passe2 = inquirer.text(
+                message="Veuillez confirmer votre mot de passe : ").execute()
+            if mot_de_passe == mot_de_passe2:
+                valide = True
+
+        # Appel de l'app pour créer l'utilisateur
+        req = requests.post(f"{host}{END_POINT}/creation/{pseudo}/{mot_de_passe}")
+
+        if req.status_code == 200:
+            message = f"Votre compte {pseudo} a été créé. Vous pouvez maintenant vous connecter."
+        else:
+            message = f"{req.status_code} {req.text} Erreur de connexion (pseudo ou mdp invalide)"
+
+        from view.accueil.accueil_vue import AccueilVue
+
+        return AccueilVue(message)
